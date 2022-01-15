9af9db683da3b4c40b3ff17b793d15cc4778cb0e
y.l <julyzergcn1@gmail.com>
2022-01-15 22:47:28 +0800

    install mysql

--- src/prj/local_settings.py
+++ src/prj/local_settings.py
@@ -1,7 +1,25 @@
+import os
+
 DEBUG = True
 # DEBUG = False
 
 ALLOWED_HOSTS = ['*']
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'db_name',
+        'USER': 'root',
+        'PASSWORD': os.environ.get('DJANGO_DB_PASSWORD', ''),
+        'HOST': 'localhost',
+        'PORT': '3306',
+    }
+}
+
+CORS_ALLOW_ALL_ORIGINS = False
+CORS_ALLOWED_ORIGINS = [
+    'http://localhost:4200'
+]
 
 REST_FRAMEWORK = {
     'DEFAULT_RENDERER_CLASSES': [
